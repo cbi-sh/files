6a187af7e3f83e4f5affbb1a7d57e04af957daa7
yykhomenko <yykhomenko@gmail.com>
2020-06-14 16:56:51 +0300

    ref deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,19 @@
-host="cbi-test1"
-user="dev"
-cbi="/store/app/src/cbi-sh"
+host="mac.local"
+user="yykhomenko"
+appHome="/Users/${user}/app/src/github.com/cbi-sh/files"
 
-ssh -o StrictHostKeyChecking=no ${user}@${host} hostname
-ssh ${user}@${host} cat /etc/*-release | grep PRETTY_NAME
-ssh ${user}@${host} lscpu | egrep 'Model name|Socket|Thread|NUMA|CPU\(s\)'
-ssh ${user}@${host} free -h
-ssh ${user}@${host} df -h /store
-ssh ${user}@${host} ulimit -aH
-echo "================================="
-ssh ${user}@${host} mkdir -p ${cbi}
-ssh ${user}@${host} ls -la ${cbi}
-scp -r [!.]* ${user}@${host}:${cbi}
+ssh -o StrictHostKeyChecking=no ${user}@${host} \
+hostname; \
+cat /etc/*-release | grep PRETTY_NAME; \
+lscpu | egrep 'Model name|Socket|Thread|NUMA|CPU\(s\)'; \
+free -h; \
+df -h /; \
+ulimit -aH; \
+echo 'appHome: '${appHome};
+#echo "================================="
+#ssh ${user}@${host} mkdir -p ${cbi}
+#ssh ${user}@${host} ls -la ${cbi}
+#scp -r [!.]* ${user}@${host}:${cbi}
 
 #ssh dev@${host} "echo 'branch: ${env.BRANCH_NAME}, build: ${buildNumber}' > ${appHome}/${system}-${
 #      module
